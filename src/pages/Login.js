32593737a0e60871aa8e3617c2029fcc0084e384
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-03-05 20:45:53 +0530

    last

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,56 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "../axios/Axios";
+import { useRecoilState } from "recoil";
+import { Auth } from "../Atom/Atom";
+import { useNavigate } from "react-router-dom";
 export const Login = () => {
+  const navigate=useNavigate();
+  const [email, setEmail] = useRecoilState(Auth);
+  const [password, setPassword] = useState();
+  const HandleSumbit=async(e)=>
+  {
+    e.preventDefault();
+    axios.post("login/",
+    {
+      email:email,
+      password:password
+    }).then((result)=>
+    {
+      if(result.data.status)
+      {
+        navigate("/");
+      }
+      else
+      {
+        console.log(result.data.msg)
+      }
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
   return (
     <div className="auth">
-      
-      <form>
-      <h1>Login</h1>
-        <input required type="text" placeholder='username' />
-        <input required type="password" placeholder='password' />
-        <button>Submit</button>
-        <p>This is error!!</p>
-        <span>Don't you have an account?<Link to="/Register">Register</Link></span>
+      <form onSubmit={HandleSumbit}>
+        <h1>Login</h1>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input required type="password" placeholder="password" 
+        value={password}
+        onChange={(e)=>{setPassword(e.target.value)}}
+          />
+        <input type="submit"/>
+        <span>
+          Don't you have an account?<Link to="/Register">Register</Link>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,69 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import axios from "../axios/Axios";
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { Auth } from "../Atom/Atom";
+import { useNavigate } from "react-router-dom";
+
 export const Register = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useRecoilState(Auth);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const HandleSumbit = async (e) => {
+    e.preventDefault();
+    console.log(name);
+    await axios
+      .post("/register", {
+        name: name,
+        email: email,
+        password: password
+      })
+      .then((result) => {
+        console.log(result.data)
+        if (result.data.status) {
+          navigate("/");
+        } else {
+          console.log("Invalid");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="auth">
-      
-      <form>
-      <h1>Signup</h1>
-        <input required type="text" re placeholder='username' />
-        <input required type="email" placeholder='Email' />
-        <input required type="password" placeholder='password' />
-        
-        <button>Submit</button>
+      <form onSubmit={HandleSumbit}>
+        <h1>Signup</h1>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          required
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input type="submit" />
         <p>This is error!!</p>
-        <span>Have an account?<Link to="/Login">Register</Link></span>
+        <span>
+          Have an account?<Link to="/Login">Register</Link>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
